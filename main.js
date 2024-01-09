1bf5abd935da9c31477a7a42a81150e454b0edaa
Smruttii24 <147795649+Smruttii24@users.noreply.github.com>
2024-01-09 11:58:58 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed= new Typed(".text",{ strings:["Beginner at Web Development","Budding Programmer","Wannabe Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
